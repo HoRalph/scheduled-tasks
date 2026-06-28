59ffd1e386fa452a0b0b0e7b16ced4858642c5d6
Ralph Ho <Ralph.Ho94@gmail.com>
2026-06-27 20:09:10 -0400

    updated csv main

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-import os
 import smtplib
 import datetime as dt
 import random
-my_email = "rho416341@gmail.com"
-password = "tqnqrmxmjesqgvpw"
-'''
-connection = smtplib.SMTP("smtp.gmail.com", port = 587)
-connection.starttls()
-connection.login(user= my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="ralph.ho94@gmail.com",msg = "Hello")
-connection.close()
-'''
+import csv
+import os
 
-with open("quotes.txt") as file:
-    all_quotes = file.readlines()
+my_email = os.environ.get("MY_EMAIL")
+password = os.environ.get("MY_PASSWORD")
+
+birthdays = []
+with open("birthdays.csv", "r") as file:
+    data = csv.DictReader(file)
+    for line in data:
+        birthdays.append(line)
 
 
 now = dt.datetime.now()
-day_of_week = now.weekday()
-print(day_of_week)
-if day_of_week == 0:
-    selected_quote = random.choice(all_quotes)
-    print(selected_quote)
-    with smtplib.SMTP("smtp.gmail.com", port = 587) as connection:
-        connection.starttls()
-        connection.login(user= my_email, password=password)
-        connection.sendmail(from_addr=my_email, to_addrs="ralph.ho94@gmail.com",msg = f"subject: Quote of the day \n\n {selected_quote}")
+letter_templates = os.listdir("./letter_templates")
 
+
+for birthday in birthdays:
+    if int(birthday["month"]) == now.month and int(birthday["day"]) == now.day:
+        letter = random.choice(letter_templates)
+        letter_lines = []
+        letter_string =""
+        with open(f"./letter_templates/{letter}") as letter_template:
+            for line in letter_template:
+                line = line.replace("[NAME]", birthday["name"])
+                letter_string += line
+        with smtplib.SMTP("smtp.gmail.com", port = 587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,to_addrs=birthday["email"], msg=f"Subject: Happy Birthday {birthday["name"]}! \n\n {letter_string}")
